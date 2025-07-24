43880dfc599faf357cc26ea14d830b85d96019d1
Syed Raza Ali <asyedraza85632@gmail.com>
2025-07-24 17:01:37 +0500

    Pushing the complete web

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
+import Benefits from "./components/Benefits.jsx";
+import Collaboration from "./components/Collaboration.jsx";
+import Footer from "./components/Footer.jsx";
+import Header from "./components/Header.jsx";
+import Hero from "./components/Hero.jsx";
+import Pricing from "./components/Pricing.jsx";
+import Roadmap from "./components/Roadmap.jsx";
+import Services from "./components/Services.jsx";
+
+
+function App() {
+
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] oberflow-hidden">
+
+        <Header />
+        <Hero/>
+        <ButtonGradient />
+        <Benefits />
+        <Collaboration />
+        <Services />
+       <Pricing />
+       <Roadmap />
+       <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
